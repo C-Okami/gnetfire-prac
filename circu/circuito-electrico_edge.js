96cc3dd0cb2543706c1c7387152979004ad713b6
Alumno <Alumno@iMac-de-Sistemas-2.local>
2019-07-08 09:42:52 -0500

    animacion con an

---
+++ circu/circuito-electrico_edge.js
@@ -0,0 +1,35 @@
+
+(function(compId){var _=null,y=true,n=false,x14='break-word',e32='${circuit_electron}',x37='100%',x1='5.0.1',x25='circuit_electron',x28='32px',x12='27',x5='horizontal',i='none',lf='left',x43='113px',e40='${Rectangle}',x29='auto',x39='rgba(223,76,20,1.00)',x21='rgba(255,255,255,1)',tp='top',e23='${aguja-Voltimetro-08}',x19='400',x30='0.4',zy='scaleY',x15='nowrap',x2='5.0.0',x6='rgba(0,0,0,0)',x35='45',x38='Rectangle',zx='scaleX',x4='both',x34='67px',x48='1',x45='0',e51='${circuit_prendida}',x33='7px',x10='rgba(8,177,20,1.00)',m='rect',x26='0px',x27='28px',x16='rgba(8,47,177,1.00)',x3='5.0.1.386',p='px',o='opacity',x47='circuit_apagada',rz='rotateZ',x36='50%',e50='${circuit_apagada}',l='normal',x13='Verdana, Geneva, sans-serif',x44='circuit_prendida',g='image',x20='none solid rgb(255, 255, 255)',e24='${info-circu}',x42='68px',x='text',x18='20',x41='1px',xc='rgba(0,0,0,1)',e22='${electron}';var g7='circuit_circuito.png',g49='circuit_apagada.png',g8='Voltimetro-08.png',g46='circuit_prendida.png',g31='circuit_electron.png',g9='Voltimetro-07.png';var s11="On",s17="Al cerrar el circuito <br>y pasar energia la <br>bombilla se enciende.<br><br>Continuar";var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:x4,cg:x5,rI:n,cn:{dom:[{id:'circuit_circuito',t:g,r:['210px','199px','603px','358px','auto','auto'],f:[x6,im+g7,'0px','0px']},{id:'swich',symbolName:'swich',t:m,r:['778','335','7','67','auto','auto']},{id:'aguja-Voltimetro-08',t:g,r:['469px','216px','55px','168px','auto','auto'],f:[x6,im+g8,'0px','0px'],tf:[[],['104'],[],['0.4','0.4'],['50.4668%','83.119%']]},{id:'Voltimetro-07',t:g,r:['286px','162px','414px','384px','auto','auto'],f:[x6,im+g9,'0px','0px'],tf:[[],[],[],['0.4','0.4']]},{id:'electron',symbolName:'electron',t:m,r:['409px','475px','28','32','auto','auto']},{id:'boton_on',t:'group',r:['832','441','68','67','auto','auto'],cu:'pointer',c:[{id:'Ellipse',t:'ellipse',r:['0px','0px','68px','67px','auto','auto'],br:["50%","50%","50%","50%"],f:[x10],s:[0,"rgb(0, 0, 0)",i]},{id:'Text',t:x,r:['15px','16px','auto','auto','auto','auto'],text:s11,n:[x13,[x12,p],"rgba(255,255,255,1.00)",l,i,"",x14,x15]}]},{id:'bombilla',symbolName:'bombilla',t:m,r:['462','123','68','113','auto','auto']},{id:'info-circu',t:'group',r:['576','57','242','135px','auto','auto'],cu:'pointer',o:'0',c:[{id:'Rectangle2',t:m,r:['0px','0px','242px','135px','auto','auto'],f:[x16],s:[0,"rgb(0, 0, 0)",i]},{id:'Text2',t:x,r:['15px','8px','auto','auto','auto','auto'],text:s17,align:"left",n:[x13,[x18,p],"rgba(255,255,255,1)",x19,x20,l,x14,x15]}]}],style:{'${Stage}':{isStage:true,r:['null','null','1024px','600px','auto','auto'],overflow:'hidden',f:[x21]}}},tt:{d:7500,a:y,data:[["eid45",lf,0,1071,"linear",e22,'596px','768px'],["eid54",lf,2143,2143,"linear",e22,'768px','217px'],["eid59",lf,5357,2143,"linear",e22,'217px','409px'],["eid68",rz,0,7500,"easeInOutBounce",e23,'104deg','-103deg'],["eid73",o,0,0,"easeInOutBounce",e24,'0','0'],["eid75",o,2000,993,"linear",e24,'0.000000','1'],["eid52",tp,1071,1071,"linear",e22,'476px','224px'],["eid56",tp,4286,1071,"linear",e22,'226px','475px'],["eid61","tr",1607.1428571429,function(e,d){this.eSA(e,d);},['play','${electron}',[]]],["eid62","tr",3214.2857142857,function(e,d){this.eSA(e,d);},['play','${electron}',[]]],["eid63","tr",4500,function(e,d){this.eSA(e,d);},['play','${electron}',[]]],["eid64","tr",6000,function(e,d){this.eSA(e,d);},['play','${electron}',[]]]]}},"electron":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:g,id:x25,r:[x26,x26,x27,x28,x29,x29],tf:[[],[],[],[x30,x30]],f:[x6,im+g31,x26,x26]}],style:{'${symbolSelector}':{r:[_,_,x27,x28]}}},tt:{d:1500,a:y,data:[["eid8",zx,0,375,"linear",e32,'0.4','1'],["eid12",zx,375,375,"linear",e32,'1','1.5'],["eid23",zx,750,375,"linear",e32,'1.5','1'],["eid22",zx,1125,375,"linear",e32,'1','0.4'],["eid9",zy,0,375,"linear",e32,'0.4','1'],["eid13",zy,375,375,"linear",e32,'1','1.5'],["eid25",zy,750,375,"linear",e32,'1.5','1'],["eid24",zy,1125,375,"linear",e32,'1','0.4']]}},"swich":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x26,x26,x33,x34,x29,x29],tf:[[],[x35],[],[],[x36,x37]],id:x38,s:[0,xc,i],t:m,f:[x39]}],style:{'${symbolSelector}':{r:[_,_,x33,x34]}}},tt:{d:1000,a:n,data:[["eid66",rz,0,1000,"linear",e40,'45deg','0deg']]}},"bombilla":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x41,x26,x42,x43,x29,x29],id:x44,o:x45,t:g,f:[x6,im+g46,x26,x26]},{r:[x26,x26,x42,x43,x29,x29],id:x47,o:x48,t:g,f:[x6,im+g49,x26,x26]}],style:{'${symbolSelector}':{r:[_,_,x42,x43]}}},tt:{d:1000,a:n,data:[["eid85",o,0,0,"linear",e50,'1','1'],["eid82",o,500,500,"linear",e50,'1','0'],["eid84",o,0,500,"linear",e51,'0','0.000000'],["eid79",o,500,500,"linear",e51,'0.000000','1']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-30029762");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${boton_on}","click",function(sym,e){sym.getSymbol("swich").play();sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3176,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${info-circu}","click",function(sym,e){sym.getSymbol("bombilla").play();sym.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'electron'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1500,function(sym,e){sym.getComposition().getStage().getSymbol("electron").play();});
+//Edge binding end
+})("electron");
+//Edge symbol end:'electron'
+
+//=========================================================
+
+//Edge symbol: 'swich'
+(function(symbolName){})("swich");
+//Edge symbol end:'swich'
+
+//=========================================================
+
+//Edge symbol: 'bombilla'
+(function(symbolName){})("bombilla");
+//Edge symbol end:'bombilla'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-30029762");
